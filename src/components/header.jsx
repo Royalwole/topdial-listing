8575f71d3667f3ba31f6f1e5d3d8607f06766282
royalwole <akolawoleakinola@gmail.com>
2025-02-05 15:41:44 +0100

    spaced-header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link href="/">
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500 mr-10'>Topdial</span>
+            <span className='text-slate-500 mr-4'>Topdial</span>
             <span className='text-slate-500'>Services</span>
           </h1>
         </Link>
