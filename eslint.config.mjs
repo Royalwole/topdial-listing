b6e2e619088dd8a1779e30673397e9cc0cab3e42
royalwole <akolawoleakinola@gmail.com>
2025-02-15 18:49:52 +0100

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,27 @@
-// import js from '@eslint/js';
-// import nextPlugin from '@next/eslint-plugin-next';
-// import babelParser from '@babel/eslint-parser';
+import js from '@eslint/js';
+import nextPlugin from '@next/eslint-plugin-next';
+import babelParser from '@babel/eslint-parser';
 
-// export default [
-//   js.configs.recommended,
-//   {
-//     plugins: {
-//       '@next/next': nextPlugin
-//     },
-//     rules: {
-//       // Disable the no-unused-vars rule globally
-//       'no-unused-vars': 'off',
-//       ...nextPlugin.configs.recommended.rules,
-//       ...nextPlugin.configs['core-web-vitals'].rules
-//     },
-//     languageOptions: {
-//       parser: babelParser,
-//       parserOptions: {
-//         requireConfigFile: false,
-//         babelOptions: {
-//           presets: ['@babel/preset-react']
-//         }
-//       }
-//     }
-//   }
-// ];
+export default [
+  js.configs.recommended,
+  {
+    plugins: {
+      '@next/next': nextPlugin
+    },
+    rules: {
+      // Disable the no-unused-vars rule globally
+      'no-unused-vars': 'off',
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs['core-web-vitals'].rules
+    },
+    languageOptions: {
+      parser: babelParser,
+      parserOptions: {
+        requireConfigFile: false,
+        babelOptions: {
+          presets: ['@babel/preset-react']
+        }
+      }
+    }
+  }
+];
