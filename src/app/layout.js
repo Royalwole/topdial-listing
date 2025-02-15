54a76a0168610372771b1346028297bd6b998a63
royalwole <akolawoleakinola@gmail.com>
2025-02-15 19:09:48 +0100

    update-commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "../components/header";
 import { ClerkProvider } from '@clerk/nextjs';
 
 const geistSans = Geist({
@@ -23,7 +22,6 @@ export default function RootLayout({ children }) {
     <ClerkProvider>
       <html lang="en">
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-          <Header />
           {children}
         </body>
       </html>
