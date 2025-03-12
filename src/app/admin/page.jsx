ff80abd1a1a569c781f68f45834d534b83e07531
royalwole <akolawoleakinola@gmail.com>
2025-03-12 16:28:53 +0100

    fgh

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useAuth } from '@clerk/nextjs';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 const AdminPage = () => {
   const { isSignedIn, user } = useAuth();
